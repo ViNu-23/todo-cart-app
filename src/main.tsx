6360755c478f586ceb685f6ac2f4c1341da2b9e4
ViNu-23 <vijayvinu46@gmail.com>
2023-12-27 16:57:36 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react'
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
   </ChakraProvider>
 
 )
