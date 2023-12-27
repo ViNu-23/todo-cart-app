9fc58055bc0f83ee15f8aaa824882345199bdac4
ViNu-23 <vijayvinu46@gmail.com>
2023-12-27 22:52:11 +0530

    fix qty

--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -29,6 +29,7 @@ import {
     const [products, setProducts] = useState([]);
     const [editingProduct, setEditingProduct] = useState(null);
     const [isEditModalOpen, setEditModalOpen] = useState(false);
+    const [quantityMap, setQuantityMap] = useState({}); // Map to store quantity for each product
     const toast = useToast();
   
     useEffect(() => {
@@ -43,79 +44,72 @@ import {
     };
   
     const buyProduct = (productId) => {
-        const quantityInput = document.getElementById("quantity");
-        const parsedQuantity = parseInt(quantityInput.value, 10);
-      
-        if (isNaN(parsedQuantity) || parsedQuantity <= 0) {
-          toast({
-            title: 'Quantity Error',
-            description: 'Please select a quantity greater than 0.',
-            status: 'error',
-            duration: 2000,
-            isClosable: true,
-          });
-          return;
-        }
-      
-        const productToBuy = products.find((product) => product.id === productId);
-        const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
-      
-        // Check if the product is already in the cart
-        const existingCartItem = cartItems.find((item) => item.id === productId);
-      
-        if (existingCartItem) {
-          // Update the quantity of the existing item
-          existingCartItem.quantity += parsedQuantity;
-        } else {
-          // Add a new item to the cart
-          const boughtItem = {
-            id: productToBuy.id,
-            name: productToBuy.name,
-            price: productToBuy.price,
-            quantity: parsedQuantity,
-          };
-          cartItems.push(boughtItem);
-        }
-      
-        // Update the cartItems in localStorage
-        localStorage.setItem('cartItems', JSON.stringify(cartItems));
-      
+      const quantity = quantityMap[productId] || 1; // Get quantity from the map or default to 1
+  
+      if (quantity <= 0) {
         toast({
-          title: 'Product Added',
-          description: `${parsedQuantity} ${parsedQuantity > 1 ? 'items' : 'item'} added to the cart.`,
-          status: 'success',
+          title: 'Quantity Error',
+          description: 'Please select a quantity greater than 0.',
+          status: 'error',
           duration: 2000,
           isClosable: true,
         });
-      
-        // Clear the quantity input field after buying
-        quantityInput.value = '';
-      };
-      
-      
-      
-      
+        return;
+      }
+  
+      const productToBuy = products.find((product) => product.id === productId);
+      const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  
+      const existingCartItem = cartItems.find((item) => item.id === productId);
+  
+      if (existingCartItem) {
+        existingCartItem.quantity += quantity;
+      } else {
+        const boughtItem = {
+          id: productToBuy.id,
+          name: productToBuy.name,
+          price: productToBuy.price,
+          quantity: quantity,
+        };
+        cartItems.push(boughtItem);
+      }
+  
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  
+      toast({
+        title: 'Product Added',
+        description: `${quantity} ${quantity > 1 ? 'items' : 'item'} added to the cart.`,
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      });
+  
+      // Reset the quantity for the current product to 1
+      setQuantityMap((prevQuantityMap) => ({
+        ...prevQuantityMap,
+        [productId]: '',
+      }));
+    };
+  
     const handleEditClick = (product) => {
       setEditingProduct(product);
       setEditModalOpen(true);
     };
   
     const handleEditSubmit = () => {
-        // Find the index of the editing product in the products array
-        const index = products.findIndex((product) => product.id === editingProduct.id);
-      
-        // If the product is found, update it in the state and localStorage
-        if (index !== -1) {
-          const updatedProducts = [...products];
-          updatedProducts[index] = editingProduct;
-      
-          localStorage.setItem('products', JSON.stringify(updatedProducts));
-          setProducts(updatedProducts);
-      
-          setEditModalOpen(false); // Close the modal
-          setEditingProduct(null); // Reset the editingProduct state
-        }
-      };
+      const index = products.findIndex((product) => product.id === editingProduct.id);
+  
+      if (index !== -1) {
+        const updatedProducts = [...products];
+        updatedProducts[index] = editingProduct;
+  
+        localStorage.setItem('products', JSON.stringify(updatedProducts));
+        setProducts(updatedProducts);
+  
+        setEditModalOpen(false);
+        setEditingProduct(null);
+      }
+    };
   
     return (
       <>
@@ -132,7 +126,13 @@ import {
           }}
         >
           {products.map((product) => (
-            <ChakraCard key={product.id} W={300} m={2} boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px" className='img-over'>
+            <ChakraCard
+              key={product.id}
+              W={300}
+              m={2}
+              boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              className='img-over'
+            >
               <CardBody>
                 <Image
                   className='img-prod'
@@ -161,13 +161,21 @@ import {
                     id="quantity"
                     name="quantity"
                     min="1"
+                    value={quantityMap[product.id] || ''}
                     placeholder='Qty'
-                    style={{ width: '40px', borderRadius: '8px', backgroundColor:'RGBA(0, 0, 0, 0.10)' }}
+                    style={{ width: '40px', borderRadius: '8px', backgroundColor: 'RGBA(0, 0, 0, 0.10)' }}
+                    onChange={(e) => {
+                      const newQuantityMap = {
+                        ...quantityMap,
+                        [product.id]: parseInt(e.target.value, 10),
+                      };
+                      setQuantityMap(newQuantityMap);
+                    }}
                   />
                   <Button
                     variant='solid'
                     colorScheme='blue'
-                    onClick={() => buyProduct(product.id, parseInt(document.getElementById("quantity").value, 10))}
+                    onClick={() => buyProduct(product.id)}
                   >
                     Buy
                   </Button>
